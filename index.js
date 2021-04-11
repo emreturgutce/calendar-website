5970012ca80ce690caf2598d6f6210c3c620314b
Emre Turgut <emrebursalee@gmail.com>
2021-04-11 15:24:35 +0300

    modularity, navbar, footer

--- index.js
+++ index.js
@@ -1,6 +1,26 @@
 let now = moment();
-const days = [1, 2, 3, 4, 5, 6, 0];
+let currEl;
+let todayEl;
+let counterTask = 0;
+let id = 1;
 
+const days = [1, 2, 3, 4, 5, 6, 0];
+/* Not kartlarının renkleri */
+const colors = [
+	'#b28c66',
+	'#b26666',
+	'#66b28c',
+	'#668cb2',
+	'#b266b2',
+	'#b2b266',
+	'#66B2B2',
+	'#313131',
+	'#6666b2',
+	'#B2668C',
+	'#8CB266',
+	'#99b2cc',
+	'#557fa9',
+];
 const myTable = document.querySelector('#my-table');
 const monthName = document.querySelector('#month-name');
 const myDateDay = document.querySelector('.my-date-day');
@@ -11,20 +31,98 @@ const myYear = document.querySelector('.my-year');
 const myDate = document.querySelector('.my-date');
 const table = document.querySelector('table');
 const wrapper = document.querySelector('.card-wrapper');
-
-let currEl;
-let todayEl;
-let counterTask = 0;
-
+const input = document.querySelector('.text-input');
+const nav = document.querySelector('.navigation__button');
 const monthStrg = now.locale('tr').format('MMMM');
 const dayNumberStr = now.locale('tr').format('DD');
 const dayStr = now.locale('tr').format('dddd');
+const notes = {};
 
 myDateDay.innerHTML = dayStr;
 myDateDate.innerHTML = `${dayNumberStr} ${monthStrg}`;
 
-const doc = {};
+/*
+ * Not kartını oluşturan ve notlar alanına ekleyen fonksiyon.
+ */
+const createEl = (value = '') => {
+	if (input.value.trim().length === 0 && value.length === 0) {
+		input.value = '';
+		return;
+	}
 
+	if (value.length !== 0) {
+		input.value = value;
+	}
+
+	const key = `${myDateDate.innerHTML} ${myYear.innerHTML}`.replaceAll(
+		' ',
+		'-',
+	);
+
+	if (value.length === 0 && notes[key]?.message.includes(input.value)) {
+		input.value = '';
+		return;
+	}
+
+	const el = `
+		<div id=${`card-${id}`} class="top-to-center card text-white bg-primary mb-3" style='user-select: none; background-color: ${
+		colors[Math.floor(Math.random() * colors.length)]
+	} !important;'>
+            <div
+                class="card-header d-flex justify-content-between align-items-center"
+            >
+                <span>Not ${counterTask++ + 1}</span>
+                <span class="btn">
+					<i class="fas fa-trash delete" style="margin-right: .5rem"></i>
+                    <i class="fas fa-chevron-down expand"></i>
+                </span>
+            </div>
+            <div class="card-body d-none">
+                <p class="card-text">
+                    ${input.value}
+                </p>
+            </div>
+        </div>
+    `;
+
+	wrapper.insertAdjacentHTML('beforeend', el);
+
+	if (value.length === 0) {
+		notes[key] = {
+			message: [...(notes[key] ? notes[key].message : []), input.value],
+		};
+	}
+
+	input.value = '';
+
+	const card = document.querySelector(`#card-${id}`);
+	const cardExpand = document.querySelector(`#card-${id} .expand`);
+	const cardDelete = document.querySelector(`#card-${id} .delete`);
+	const cardText = document.querySelector(`#card-${id} .card-text`);
+
+	cardExpand.addEventListener('click', () => {
+		document
+			.querySelector(
+				`#card-${+card.getAttribute('id').split('-')[1]} .card-body`,
+			)
+			.classList.toggle('d-none');
+	});
+
+	cardDelete.addEventListener('click', () => {
+		notes[key] = {
+			message: [
+				...notes[key].message.filter(
+					(d) => d !== cardText.innerHTML.replaceAll('\n', '').trim(),
+				),
+			],
+		};
+		currEl.click();
+	});
+
+	id++;
+};
+
+/* Bir önceki aya gitmek istediğinde kullanıcının tıkladığı buton */
 prev.addEventListener('click', () => {
 	now = now.clone().subtract(1, 'month');
 	month();
@@ -34,6 +132,7 @@ prev.addEventListener('click', () => {
 	}, 500);
 });
 
+/* Bir sonraki aya gitmek istediğinde kullanıcının tıkladığı buton */
 next.addEventListener('click', () => {
 	now = now.clone().add(1, 'month');
 	month();
@@ -43,10 +142,14 @@ next.addEventListener('click', () => {
 	}, 500);
 });
 
-month();
+/* Input üzerinde "enter" tuşuna basıldığında kart oluşturma fonksiyonunu çağırır. */
+input.addEventListener('keydown', (e) => {
+	if (e.key == 'Enter') {
+		createEl();
+	}
+});
 
-
-document.querySelectorAll('.daysOfMonth').forEach((el) =>{
+document.querySelectorAll('.daysOfMonth').forEach((el) => {
 	if (myDateDate.innerHTML.split(' ')[0] === el.innerHTML) {
 		currEl = el;
 		todayEl = el;
@@ -54,17 +157,29 @@ document.querySelectorAll('.daysOfMonth').forEach((el) =>{
 	}
 });
 
+document.querySelectorAll('.navigation__link').forEach((val) => {
+	val.addEventListener('click', () => {
+		nav.click();
+	});
+});
+
+month();
+
+/* Kullanıcı sağ taraftaki "Bugüne git" butonuna tıkladığında çağırılan fonksiyon */
 function onClickToday() {
 	month();
 	todayEl.classList.add('my-hover');
 	todayEl.click();
 }
+
+/* Takvim üzerinde bulunan günlerin hover stillerini silen fonksiyon */
 function clearHover() {
 	document.querySelectorAll('.daysOfMonth').forEach((el) => {
 		el.classList.remove('my-hover');
 	});
 }
 
+/* Moment kütüphanesini kullanarak takvimi dolduran fonksiyon */
 function month() {
 	const monthStr = now.locale('tr').format('MMMM');
 	const previousMonth = now.clone().subtract(1, 'months').daysInMonth();
@@ -104,7 +219,9 @@ function month() {
 			const clickedDay = +e.target.innerHTML;
 			const nowDay = +now.format('DD');
 
-			myDateDay.innerHTML = now.add(clickedDay - nowDay, 'days').format('dddd');
+			myDateDay.innerHTML = now
+				.add(clickedDay - nowDay, 'days')
+				.format('dddd');
 			myDateDate.innerHTML = `${e.target.innerHTML} ${monthStr}`;
 			myDate.classList.add('my-animation');
 			setTimeout(() => {
@@ -112,12 +229,15 @@ function month() {
 			}, 1000);
 			wrapper.innerHTML = '';
 			counterTask = 0;
-			const key = `${myDateDate.innerHTML} ${myYear.innerHTML}`.replaceAll(' ', '-'); 
-			const messages = doc[key] ? doc[key].message : [];
+			const key = `${myDateDate.innerHTML} ${myYear.innerHTML}`.replaceAll(
+				' ',
+				'-',
+			);
+			const messages = notes[key] ? notes[key].message : [];
 			messages.forEach((message) => createEl(message));
 			currEl = el;
 			currEl.classList.add('my-hover');
-		})
+		});
 	});
 
 	document.querySelectorAll('.daysOfPreviousMonth').forEach((el) => {
@@ -126,15 +246,22 @@ function month() {
 			const clickedDay = +e.target.innerHTML;
 			const nowDay = +now.format('DD');
 
-			myDateDay.innerHTML = now.add(clickedDay - nowDay, 'days').format('dddd');
-			myDateDate.innerHTML = `${e.target.innerHTML} ${now.locale('tr').format('MMMM')}`;
+			myDateDay.innerHTML = now
+				.add(clickedDay - nowDay, 'days')
+				.format('dddd');
+			myDateDate.innerHTML = `${e.target.innerHTML} ${now
+				.locale('tr')
+				.format('MMMM')}`;
 			myDate.classList.add('my-animation');
 			setTimeout(() => {
 				myDate.classList.remove('my-animation');
 			}, 1000);
 			wrapper.innerHTML = '';
-			const key = `${myDateDate.innerHTML} ${myYear.innerHTML}`.replaceAll(' ', '-'); 
-			const messages = doc[key] ? doc[key].message : [];
+			const key = `${myDateDate.innerHTML} ${myYear.innerHTML}`.replaceAll(
+				' ',
+				'-',
+			);
+			const messages = notes[key] ? notes[key].message : [];
 			messages.forEach((message) => createEl(message));
 			currEl = el;
 		});
@@ -146,118 +273,24 @@ function month() {
 			const clickedDay = +e.target.innerHTML;
 			const nowDay = +now.format('DD');
 
-			myDateDay.innerHTML = now.add(clickedDay - nowDay, 'days').format('dddd');
-			myDateDate.innerHTML = `${e.target.innerHTML} ${now.locale('tr').format('MMMM')}`;
+			myDateDay.innerHTML = now
+				.add(clickedDay - nowDay, 'days')
+				.format('dddd');
+			myDateDate.innerHTML = `${e.target.innerHTML} ${now
+				.locale('tr')
+				.format('MMMM')}`;
 			myDate.classList.add('my-animation');
 			setTimeout(() => {
 				myDate.classList.remove('my-animation');
 			}, 1000);
 			wrapper.innerHTML = '';
-			const key = `${myDateDate.innerHTML} ${myYear.innerHTML}`.replaceAll(' ', '-'); 
-			const messages = doc[key] ? doc[key].message : [];
+			const key = `${myDateDate.innerHTML} ${myYear.innerHTML}`.replaceAll(
+				' ',
+				'-',
+			);
+			const messages = notes[key] ? notes[key].message : [];
 			messages.forEach((message) => createEl(message));
 			currEl = el;
 		});
 	});
 }
-
-const colors = [
-	'#b28c66',
-	'#b26666',
-	'#66b28c',
-	'#668cb2',
-	'#b266b2',
-	'#b2b266',
-	'#66B2B2',
-	'#313131',
-	'#6666b2',
-	'#B2668C',
-	'#8CB266',
-	'#99b2cc',
-	'#557fa9',
-];
-
-const input = document.querySelector('input');
-
-input.addEventListener('keydown', (e) => {
-	if (e.key == 'Enter') {
-		createEl();
-	}
-});
-
-let id = 1;
-
-
-document.querySelector('#card-0').addEventListener('click', () => {
-	document.querySelector('#card-0 .card-body').classList.toggle('d-none');
-});
-
-const createEl = (value = '') => {
-	if (input.value.trim().length === 0 && value.length === 0) {
-		input.value = '';
-		return;
-	}
-
-	if (value.length !== 0) {
-		input.value = value;
-	}
-	
-	const key = `${myDateDate.innerHTML} ${myYear.innerHTML}`.replaceAll(' ', '-'); 
-
-	if (value.length === 0 && doc[key]?.message.includes(input.value)) {
-		input.value = '';
-		return;
-	}
-
-	const el = `
-		<div id=${`card-${id}`} class="top-to-center card text-white bg-primary mb-3" style='user-select: none; background-color: ${
-		colors[Math.floor(Math.random() * colors.length)]
-	} !important;'>
-            <div
-                class="card-header d-flex justify-content-between align-items-center"
-            >
-                <span>Not ${(counterTask++) + 1}</span>
-                <span class="btn">
-					<i class="fas fa-trash delete" style="margin-right: .5rem"></i>
-                    <i class="fas fa-chevron-down expand"></i>
-                </span>
-            </div>
-            <div class="card-body d-none">
-                <p class="card-text">
-                    ${input.value}
-                </p>
-            </div>
-        </div>
-    `;
-
-	wrapper.insertAdjacentHTML('beforeend', el);
-
-	
-	if (value.length === 0) {
-		doc[key] = {
-			message: [...(doc[key] ? doc[key].message : []), input.value],
-		};
-	}
-
-	input.value = '';
-
-	const card = document.querySelector(`#card-${id}`);
-	const cardExpand = document.querySelector(`#card-${id} .expand`);
-	const cardDelete = document.querySelector(`#card-${id} .delete`);
-	const cardText = document.querySelector(`#card-${id} .card-text`);
-
-	cardExpand.addEventListener('click', () => {
-		document
-			.querySelector(
-				`#card-${+card.getAttribute('id').split('-')[1]} .card-body`,
-			)
-			.classList.toggle('d-none');
-	});
-	
-	cardDelete.addEventListener('click', () => {
-		doc[key] = { message: [...doc[key].message.filter((d) => d !== cardText.innerHTML.replaceAll('\n', '').trim())]}
-		currEl.click();
-	});
-
-	id++;
-};
